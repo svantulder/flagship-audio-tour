c726dac67558feeba90ab91a5867652182bdba44
svantulder <stefan@talentdatalabs.com>
2026-05-30 10:56:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
-const CACHE_NAME = 'flagship-tour-v1';
-
 // List every static asset and audio file your app needs to run offline
+const CACHE_NAME = 'flagship-tour-v3'; // Must bump to v3
+
+// Notice the dots added before every slash
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json',
-    // You must list the exact filenames of your MP3s here
-    '/VOC.mp3',
-    '/grachtengordel.mp3',
-    '/houseboats.mp3',
-    '/dancing_houses.mp3',
-    '/anne_frank.mp3',
-    '/jordaan.mp3',
-    '/westerkerk.mp3',
-    '/leidseplein.mp3',
-    '/vondelpark.mp3',
-    '/museumplein.mp3',
-    '/bicycles.mp3',
-    '/three_crosses.mp3'
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './logo.svg',
+    './VOC.mp3',
+    './grachtengordel.mp3',
+    './houseboats.mp3',
+    './dancing_houses.mp3',
+    './anne_frank.mp3',
+    './jordaan.mp3',
+    './westerkerk.mp3',
+    './leidseplein.mp3',
+    './vondelpark.mp3',
+    './museumplein.mp3',
+    './bicycles.mp3',
+    './three_crosses.mp3',
+    './Damsquare.mp3'
 ];
 
 // 1. Install Event: Cache all assets
